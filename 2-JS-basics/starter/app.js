2fd63f2d19330674c1079e7a94a12c96cf41223d
Chijioke Nwagwu <wyhzest@gmail.com>
2019-04-19 10:50:12 +0100

    implements an age calculator

--- 2-JS-basics/starter/app.js
+++ 2-JS-basics/starter/app.js
@@ -1,4 +1,5 @@
 // Variable declaration
+/*
 let firstName = prompt('what is your name?');
 let LastName  = "Nwagwu";
 let age = prompt('how old are you?');
@@ -6,7 +7,18 @@ let fullAge = true;
 let job;
 job = prompt('what is your occupation?');
 alert(firstName + ' is a ' + age + ' years old ' + job);
-confirm('would you like to proceed?')
+//confirm('would you like to proceed?')
 //alert(job);
 
 // TO DO: play with alert, prompt and confirm.
+*/
+let currentYear, firstName, LastName, birthYear, age;
+
+firstName = prompt('What is your name?');
+birthYear = prompt('When weere you born?');
+currentYear = prompt('What year are we in');
+
+age = parseInt(currentYear) - parseInt(birthYear);
+confirm(firstName + ' I know your age');
+
+alert('You are ' + age + ' years old!')
