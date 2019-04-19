ac1573cf1e164f19065a58dc2cc4a04fe486cbe4
Chijioke Nwagwu <wyhzest@gmail.com>
2019-04-19 08:21:51 +0100

    tests type coercion

--- 2-JS-basics/starter/app.js
+++ 2-JS-basics/starter/app.js
@@ -1,8 +1,9 @@
+// Variable declaration
 let firstName = "Chijioke";
 let LastName  = "Nwagwu";
 let age = 32;
 let fullAge = true;
 let job;
 job = "Programmer";
-alert(firstName);
+alert(firstName + ' ' + age);
 alert(job);
