91e5342e459498661495a4aef55e62161811fcb5
Chijioke Nwagwu <wyhzest@gmail.com>
2019-04-19 10:31:50 +0100

    variable assignment using prompt

--- 2-JS-basics/starter/app.js
+++ 2-JS-basics/starter/app.js
@@ -1,9 +1,12 @@
 // Variable declaration
-let firstName = "Chijioke";
+let firstName = prompt('what is your name?');
 let LastName  = "Nwagwu";
-let age = 32;
+let age = prompt('how old are you?');
 let fullAge = true;
 let job;
-job = "Programmer";
-alert(firstName + ' ' + age);
-alert(job);
+job = prompt('what is your occupation?');
+alert(firstName + ' is a ' + age + ' years old ' + job);
+confirm('would you like to proceed?')
+//alert(job);
+
+// TO DO: play with alert, prompt and confirm.
