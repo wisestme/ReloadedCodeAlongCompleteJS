347f8bdc56d3afb22eab376517d55c620f1d5b7f
Chijioke Nwagwu <wyhzest@gmail.com>
2019-04-29 08:03:00 +0100

    assigns a DOM element content to a variable

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -16,5 +16,9 @@ activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+let x = document.querySelector('#score-0').textContent;
+
+
