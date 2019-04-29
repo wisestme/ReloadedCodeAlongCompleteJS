4a950a1fe8be613982f51e71410977e449cae96d
Chijioke Nwagwu <wyhzest@gmail.com>
2019-04-29 16:16:36 +0100

    adds event listener to change dice value when roll dice is clicked

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -14,7 +14,6 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
@@ -22,3 +21,7 @@ document.querySelector('#current-' + activePlayer).textContent = dice;
 let x = document.querySelector('#score-0').textContent;
 
 document.querySelector('.dice').style.display = 'none'
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	dice = Math.floor(Math.random() * 6) + 1;
+})
