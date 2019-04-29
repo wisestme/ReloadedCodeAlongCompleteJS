75739d2f512bdd70079b1b6b579c689640dceb4a
Chijioke Nwagwu <wyhzest@gmail.com>
2019-04-29 07:54:56 +0100

    styles the dice value

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -16,4 +16,5 @@ activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
