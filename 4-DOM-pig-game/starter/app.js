0cd30c60dd29d7c6036d924bbbc1c7cf94d8e010
Chijioke Nwagwu <wyhzest@gmail.com>
2019-04-30 01:21:37 +0100

    resets round score to 0 when dice is value of 1

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -44,6 +44,12 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		roundScore += dice;
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	} else {
-		activePlayer = 0 ? activePlayer += 1 : activePlayer = 0;
+		roundScore = 0;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+		//activePlayer === 0 ? activePlayer += 1 : activePlayer = 0;
+		//roundScore = 0;
+		//roundScore += dice;
+		//document.querySelector('#current-' + activePlayer).textContent = roundScore;
+		//document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	}
 });
