9a508e5da09aadc8da9978daebd035a8787af636
Chijioke Nwagwu <wyhzest@gmail.com>
2019-05-23 19:07:31 +0100

    compares data mutation in function arguments of primitives wrt objects

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -38,6 +38,7 @@ john.job = 'teacher';*/
 
 // objects vs primitives
 
+// primitives
 let a = 23;
 let b = a;
 
@@ -45,6 +46,7 @@ a = 32;
 
 console.log(a, b);
 
+// objects
 let obj1 = {
 	name: 'John',
 	age: 26,
@@ -54,3 +56,19 @@ let obj2 = obj1;
 obj1.age = 30;
 
 console.log(obj1, obj2);
+
+// Functions
+let age = 27;
+let obj = {
+	name:'Jonas',
+	city: 'Lisbon'
+};
+
+function change(a, b) {
+	a = 30;
+	b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age, obj.city);
