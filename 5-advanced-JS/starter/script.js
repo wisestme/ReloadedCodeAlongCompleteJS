a99c203fef800bb3348c66733ff76ec5dd7226f3
wisestMe <wyhzest@gmail.com>
2019-06-05 12:05:45 +0100

    creates a retirement function

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -156,12 +156,5 @@ interviewQuestion()('wisest');
 })(3);*/
 
 function retirement (retirementAge) {
-	let a = ' years left until retirement';
-	return function (yearOfBirth) {
-		let age = 2019 - yearOfBirth;
-		console.log((retirementAge - age) + a);
-	}
+	
 }
-
-let retirementUS = retirement(66);
-retirementUS(1987);
