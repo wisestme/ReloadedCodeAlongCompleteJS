1113cd71ab77a8bc127b819c9b5624f84dff4292
wisestMe <wyhzest@gmail.com>
2019-06-13 15:59:22 +0100

    adds arguement to function

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -388,7 +388,7 @@ let answer = arrayOp(older, message);*/
 
 interviewQuestion('Programmer')('Chijioke');*/
 
-(function () {
+(function (goodluck) {
 	let score = Math.random() * 10;
 	console.log(score >= 5);
-})()
+})();
